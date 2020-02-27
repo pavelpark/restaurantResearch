f48b029bdd985fe29019d62697f3d52332fc935a
Pavel Parkhomey <pavelpark@aol.com>
2020-02-27 13:25:59 -0800

    Displaying restaurant images, stars, and reviews

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -4,11 +4,26 @@ import { View, Image, Text, StyleSheet } from 'react-native';
 const ResultsDetail = ({ result }) => {
     return (
         <View>
-            <Text>{result.name}}</Text>
+            <Image style={styles.imageStyle}
+                source={{ uri: result.image_url }} />
+            <Text style={styles.restaurantNameStyle}>
+                {result.name}}
+            </Text>
+            <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
         </View>
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    imageStyle: {
+        width: 250,
+        height: 120,
+        borderRadius: 4
+    },
+    restaurantNameStyle: {
+        fontWeight: 'bold',
+
+    }
+});
 
 export default ResultsDetail;
