884c4183191f11b34fa17639a1e0a5a8e45e2088
Pavel Parkhomey <pavelpark@aol.com>
2020-02-27 14:52:23 -0800

    Displaying content on ResultsShowScreen

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
 import ResultsDetail from './ResultsDetail';
 
-
 const ResultsList = ({ title, results, navigation }) => {
+    if (!results.length) {
+        return null;
+    }
+
     return (
         <View style={styles.containerStyle}>
             <Text style={styles.titleStyle}>{title}</Text>
@@ -15,7 +19,7 @@ const ResultsList = ({ title, results, navigation }) => {
                 keyExtractor={(results) => result.id}
                 renderItem={({ item }) => {
                     return (
-                        <TouchableOpacity onPress={() => navigation.navigate('ResultsShow')}>
+                        <TouchableOpacity onPress={() => navigation.navigate('ResultsShow', { id: item.id })}>
                             <ResultsDetail result={item} />
                         </TouchableOpacity>
                     )
@@ -37,4 +41,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import yelp from '../api/yelp';
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+    const [result, setResult] = useState(null);
+    const id = navigation.getParam('id');
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`)
+        setResult(response.data)
+    };
+    useEffect(() => {
+        getResult(id);
+    }, []);
+
+    if (!result) {
+        return null;
+    };
+
     return (
         <View>
-            <Text>Results Show</Text>
+            <Text>{result.photos}</Text>
+            <FlatList
+                data={result.photos}
+                keyExtractor={(photo) => photo}
+                renderItem={(item) => {
+                    return <Image style={styles.imageStyle} source={{ uri: item }} />
+                }}
+            />
         </View>
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    imageStyle: {
+        hight: 200,
+        width: 300
+    }
+});
 
 export default ResultsShowScreen;
